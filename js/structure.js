5e3ab3e4f8f3bc6f3ac4f16373f5b466737b5f30
Ishanka Madusanka <66905286+Ishanka-M@users.noreply.github.com>
2026-03-02 15:34:26 +0530

    Update structure.js

--- js/structure.js
+++ js/structure.js
@@ -1,1 +1,18 @@
+function detectStructure(data){
+    if(data.length < 5) return "neutral";
 
+    let highs=[], lows=[];
+    data.slice(-20).forEach(c=>{
+        highs.push(c.high);
+        lows.push(c.low);
+    });
+
+    const hh = highs[highs.length-1] > highs[highs.length-2];
+    const hl = lows[lows.length-1] > lows[lows.length-2];
+    const lh = highs[highs.length-1] < highs[highs.length-2];
+    const ll = lows[lows.length-1] < lows[lows.length-2];
+
+    if(hh && hl) return "bull";
+    if(lh && ll) return "bear";
+    return "neutral";
+}
